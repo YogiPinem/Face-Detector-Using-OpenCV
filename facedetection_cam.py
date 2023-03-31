bfd72a1bd3db409d8ac769d2f2e224a3f0a2b81b
Yogi Immanuel Pinem <86791091+YogiPinem@users.noreply.github.com>
2023-04-01 01:06:05 +0700

    Update facedetection_cam.py

--- facedetection_cam.py
+++ facedetection_cam.py
@@ -1,13 +1,12 @@
 import cv2
 import sys
 
-cascPath = "UTS\KSI\haarcascade_frontalface_default.xml"
+cascPath = "haarcascade_frontalface_default.xml"
 wajah = cv2.CascadeClassifier(cascPath)
 
 video_capture = cv2.VideoCapture(0)
 font = cv2.FONT_HERSHEY_SIMPLEX
 while True:
-    # Capture frame-by-frame
     ret, frame = video_capture.read()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -20,16 +19,13 @@ while True:
         flags=cv2.CASCADE_SCALE_IMAGE
     )
 
-    # Draw a rectangle around the deteksi_wajah
     for (x, y, w, h) in deteksi_wajah:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-    # Display the resulting frame
     cv2.imshow('Video', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# When everything is done, release the capture
 video_capture.release()
 cv2.destroyAllWindows()
